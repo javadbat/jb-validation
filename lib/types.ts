5c85f5c1f7b688b32a55baf544ddc0a56ae27e3b
javadbat <javadbat@gmail.com>
2025-03-25 01:11:17 +0330

    add persist option to show and clear validation error methods type

--- lib/types.ts
+++ lib/types.ts
@@ -2,63 +2,78 @@ import { type ValidationHelper } from "./validation-helper";
 
 //all web component that has validation will implements this interface
 export interface WithValidation<TValidationValue = any> {
-    readonly isAutoValidationDisabled: boolean,
-    readonly validation: ValidationHelper<TValidationValue>,
-    required: boolean,
-    validationMessage: string
-    showValidationError: ShowValidationErrorCallback | ((error: string) => void),
-    clearValidationError: ClearValidationErrorCallback,
-    checkValidity: () => boolean
-    reportValidity: () => boolean
+  readonly isAutoValidationDisabled: boolean,
+  readonly validation: ValidationHelper<TValidationValue>,
+  required: boolean,
+  validationMessage: string
+  showValidationError: ShowValidationErrorCallback | ((error: string) => void),
+  clearValidationError: ClearValidationErrorCallback,
+  checkValidity: () => boolean
+  reportValidity: () => boolean
 }
 
 export type ValidationResultSummary = {
-    isValid: boolean | null;
-    message: string | null;
+  isValid: boolean | null;
+  message: string | null;
 }
 export type ValidationResultItem<ValidationValue> = {
-    isValid: boolean | null;
-    message: string | null;
-    validation: ValidationItem<ValidationValue>;
+  isValid: boolean | null;
+  message: string | null;
+  validation: ValidationItem<ValidationValue>;
 }
 export type ValidationResult<ValidationValue> = {
-    validationList: ValidationResultItem<ValidationValue>[];
-    isAllValid: boolean;
+  validationList: ValidationResultItem<ValidationValue>[];
+  isAllValid: boolean;
 }
 export type ValidatorFunction<ValidationValue> = (value: ValidationValue) => boolean | string | Promise<boolean | string>
 //TValue will be different for some inputs like date input
 export type ValidationItem<ValidationValue> = {
-    /**
-     * @property validation function or regex to match value with
-     */
-    validator: RegExp | ValidatorFunction<ValidationValue>;
-        /**
-     * @property error message that will be shown if validator return false or regex failed
-     */
-    message: string;
-    /**
-     *  @property type of error (category) its optional
-     */
-    stateType?: keyof ValidityStateFlags;
-    /**
-     * @property defer validator execution until all non-deferred validation executed.(good for async validator)
-     */
-    defer?:boolean
+  /**
+   * @property validation function or regex to match value with
+   */
+  validator: RegExp | ValidatorFunction<ValidationValue>;
+  /**
+* @property error message that will be shown if validator return false or regex failed
+*/
+  message: string;
+  /**
+   *  @property type of error (category) its optional
+   */
+  stateType?: keyof ValidityStateFlags;
+  /**
+   * @property defer validator execution until all non-deferred validation executed.(good for async validator)
+   */
+  defer?: boolean
 }
 export type checkValidityInput<ValidationValue> = {
-    /**
-     * @property showError determine if you want to show error in invalidate state or not. default is true
-     */
-    showError?: boolean,
-        /**
-     * @property the value you want to check validation on its optional and if not set it will get from component value)
-     */
-    value?: ValidationValue
+  /**
+   * @property showError determine if you want to show error in invalidate state or not. default is true
+   */
+  showError?: boolean,
+  /**
+* @property the value you want to check validation on its optional and if not set it will get from component value)
+*/
+  value?: ValidationValue
+}
+//Error showing and clearing mechanism types
+export type ShowValidationErrorParameters = {
+  message: string,
+  /**
+   * @property normal error will be cleared after type or blur event, but with persist set to true error will not be cleared until clearValidationError called
+  */
+ persist?: boolean
+}
+export type ClearValidationErrorParameters = {
+  /**
+   * @property clear persisted error (error that set with persist flag) if not set all errors will be cleared 
+  */
+ clearPersist?: boolean
+}
+export type PersistedError= {
+  message:string
 }
 //callbacks
-export type ShowValidationErrorInput = { message: string }
-//
-export type ShowValidationErrorCallback = (error: ShowValidationErrorInput) => void;
+export type ShowValidationErrorCallback = (error: ShowValidationErrorParameters) => void;
 export type ClearValidationErrorCallback = () => void;
 export type GetValueCallback<ValidationValue> = () => (ValidationValue | Promise<ValidationValue>);
 export type GetValueStringCallback<ValidationValue> = (value: ValidationValue) => string;
@@ -66,13 +81,13 @@ export type GetValidationsCallback<ValidationValue> = () => ValidationItem<Valid
 export type SetValidationResultCallback<ValidationValue> = (result: ValidationResult<ValidationValue>) => void;
 //
 export type Callbacks<ValidationValue> = {
-    getValue: GetValueCallback<ValidationValue>,
-    getValueString: GetValueStringCallback<ValidationValue>,
-    showValidationError: ShowValidationErrorCallback[],
-    clearValidationError: ClearValidationErrorCallback[],
-    getValidations: GetValidationsCallback<ValidationValue>[],
-    setValidationResult: SetValidationResultCallback<ValidationValue>[]
+  getValue: GetValueCallback<ValidationValue>,
+  getValueString: GetValueStringCallback<ValidationValue>,
+  showValidationError: ShowValidationErrorCallback[],
+  clearValidationError: ClearValidationErrorCallback[],
+  getValidations: GetValidationsCallback<ValidationValue>[],
+  setValidationResult: SetValidationResultCallback<ValidationValue>[]
 }
 export type CallbacksInput<ValidationValue> = {
-    [key in keyof Callbacks<ValidationValue>]?: Callbacks<ValidationValue>[key] extends Array<any> ? Callbacks<ValidationValue>[key][0] : Callbacks<ValidationValue>[key]
+  [key in keyof Callbacks<ValidationValue>]?: Callbacks<ValidationValue>[key] extends Array<any> ? Callbacks<ValidationValue>[key][0] : Callbacks<ValidationValue>[key]
 }
